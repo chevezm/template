1155fb77d275a33e1af494443d62a701892243e6
chevezm <61294146+chevezm@users.noreply.github.com>
2024-02-13 15:12:43 -0500

    Update functions.js
    added code to enable tooltips

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,14 @@
+// Optional enable tooltips
+/*
+document.addEventListener("DOMContentLoaded", function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(element){
+        return new bootstrap.Tooltip(element);
+    });
+});
+*/
+
+
 // not suitable for large data sets
 function bubbleSort(array){ //descending sort
     for (let i = 0; i < array.length - 1; i++){
